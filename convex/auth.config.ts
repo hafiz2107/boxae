b066d03432607665948d8353e6b4cbf890caca6c
hafiz <hafiz@boundaryfill.com>
2024-04-21 19:37:02 +0530

    Added convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: 'https://nearby-newt-33.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+ 
